70e2094b722fefed9f43ca6a172ec78c95abb38b
RubystarAshe <rubystarashe@gmail.com>
2018-06-06 00:32:57 +0900

    [M] vue use electron plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,5 +3,15 @@ module.exports = {
     title: 'Electron Nuxt'
   },
 	loading: false,
-	dev: process.env.NODE_ENV === 'DEV'
+	dev: process.env.NODE_ENV === 'DEV',
+	build: {
+		extend (config, { isDev, isClient }) {
+			if (isClient) {
+				config.target = 'electron-renderer'
+			}
+		}
+	},
+	plugins: [
+		'~plugins/vue-electron.js'
+	]
 }
