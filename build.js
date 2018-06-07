c34f45453d0c2a0af4762af2d1b7eee81db9892b
RubystarAshe <rubystarashe@gmail.com>
2018-06-07 20:43:41 +0900

    [M] windows installer

--- build.js
+++ build.js
@@ -13,12 +13,11 @@ function getInstallerConfig () {
   const rootPath = path.join('./')
 
   return Promise.resolve({
-    appDirectory: path.join(rootPath, 'electron-nuxt-example-win32-x64/'),
+    appDirectory: path.join(rootPath, 'electron-nuxt-win32-x64/'),
     noMsi: true,
     outputDirectory: path.join(rootPath, 'windows-installer'),
-    loadingGif: path.join(rootPath, 'assets/installing_png.gif'),
+    loadingGif: path.join(rootPath, 'assets/installing.gif'),
     exe: 'NuxtExample.exe',
-    setupExe: 'example_installer.exe',
-    setupIcon: path.join(rootPath, 'assets', 'icons', 'icon.ico')
+    setupExe: 'example_installer.exe'
   })
 }
